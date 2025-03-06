13e81e42c3594168685720f9563afdf618ca5e34
Kar xuan <chenkarxuan@gmail.com>
2025-03-06 23:03:32 +0800

    modify project description

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -4,7 +4,7 @@ export default function Projects() {
   return (
     <main className="p-8 sm:p-20 bg-base-200 text-base-content">
       <h1 className="text-4xl font-semibold text-primary">My Projects</h1>
-      <p className="mt-4 text-lg">Check out my exciting projects below:</p>
+      <p className="mt-4 text-lg">For more information, check out my GitHub!</p>
 
       <section className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {/* BitSniffer Project */}
